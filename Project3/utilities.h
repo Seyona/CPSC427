bacc92d06c81afe406e80ddb3ff7c92fdf7f1b55
Seyon <dgbaker93@gmail.com>
2015-09-28 21:48:12 -0400

    Added utilities.h, no changes to over all structure

---
+++ Project3/utilities.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct measurement {
+	int index, value;
+	void clear() { index, value; }
+};
+
+struct stats {
+	double mean;
+	double standard_dev;
+	double totalmeasurements;
+	int totalMeasurements_withinOneStdDev;
+	int totalMeasurements_outsideOneStdDev;
+	void clear() { mean, standard_dev, totalmeasurements, totalMeasurements_withinOneStdDev, totalMeasurements_outsideOneStdDev; }
+};
+
+void readFromFile(std::string fileName, std::vector<measurement> &readings);
+double calculateMean(std::vector<measurement> readings);
+double calculateStdDev(std::vector<measurement> readings);
+int getTotalMeasurements(std::vector<measurement> readings);
+void isWithinOne(std::vector<measurement> readings, std::vector<measurement> &withinOne, std::vector<measurement> &notWithinOne, double mean);
+void sort(std::vector<measurement> &readings);
+void print_to_file(std::vector<measurement> withinOne, std::vector<measurement> notWithinOne, stats stat);
+
