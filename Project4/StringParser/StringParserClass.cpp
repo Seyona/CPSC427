11647851011c57f98d1aa2c337708a46bd6a7f49
LordFluffyPants <rixmaddi@gmail.com>
2015-10-23 14:46:03 -0400

    added implementation of string parser

--- Project4/StringParser/StringParserClass.cpp
+++ Project4/StringParser/StringParserClass.cpp
@@ -57,120 +57,42 @@ bool StringParserClass::setTags(const char *pStartTag, const char *pEndTag) {
 		Success bool
 */
 bool StringParserClass::getDataBetweenTags(char *pDataToSearchThru, vector<string> &myvector) {
-	char * pOpeningTagStart(NULL), * pOpeningTagEnd(NULL), * pClosingTagStart(NULL), * pClosingTagEnd(NULL);
-	// see if you can cast it as a string
+	int lengthOfStartTag = strlen(pStartTag)/sizeof(char);
+	int lengthOfEndTag = strlen(pEndTag)/sizeof(char);
+	int lengthOfData = strlen(pDataToSearchThru)/sizeof(char);
 
-	/*
-			These are just notes we can talk about it later...
+	bool startFound = false;
+	string stringToAdd = "";
 
-			when iterating over the data to find the start and end points of a tag let
-				Start = CurrentPointerPos
-				End = CurrentPointerPos + sizeOfTag-1
-
-				then when start is a '<' and end is a '>' we can check the inside of the tag to see if it is what we want
-				and then we search for the ending tag position the difference new start is the end of the last tag. then we collect data until we reach another 
-				scenario where start is a '<' and end is a '>' and if it is the tag we are looking for exit out we have our data. else keep going.
-	*/
-
-	if (!areTagsSet) {
-		lastError = ERROR_TAGS_NULL;
-		return false;
+	for (int i = 0; i < lengthOfData; i++)
+	{
+		if (startFound)
+		{
+			if (pEndTag[0] == *(pDataToSearchThru + i) && (memcmp(pEndTag,(pDataToSearchThru + i),lengthOfEndTag) == 0))
+			{
+				myvector.push_back(stringToAdd);
+				stringToAdd = "";
+				i += lengthOfEndTag -1;
+				startFound = false;
+			}
+			else 
+			{
+				stringToAdd += *(pDataToSearchThru + i);
+			}
+		}
+		else if (pStartTag[0] == *(pDataToSearchThru + i) && (memcmp(pStartTag,(pDataToSearchThru + i),lengthOfStartTag)))
+		{
+			i += lengthOfStartTag -1;
+			startFound = true;
+		}
 	}
-
-	while (this -> getLastError() !=  END_OF_DATA || this -> getLastError() != NO_TAG_FOUND) {
-		if ((pDataToSearchThru != NULL) | (*(pDataToSearchThru) != '\0')) {
-			pOpeningTagStart = pDataToSearchThru; //start of pDataTosearchThru
-		} else {
-			this -> lastError = ERROR_DATA_NULL;
-			return false;
-		}
-
-		int tagSize = 0;
-
-		const char* startTag = this -> pStartTag;
-		char * ptemp = pOpeningTagStart;
-
-		while ( *(startTag) != '\0') { //null char 
-			if ( *(ptemp) == '\0') {
-				this -> lastError = END_OF_DATA;
-				return false;
-			}
-			ptemp++;
-			tagSize++; //may not need tag size, keep just in case
-			startTag++;
-		}
-
-		//openingTagEnd = popeningTagSTart + (tagSize - 1)
-		pOpeningTagEnd = ptemp; 
-		ptemp = NULL;
-		// might need check if pOpeningTagEnd is getting set to a valid character... Throw datanull error if not? 
-
-		bool good = this -> findTag( this->pStartTag, pOpeningTagStart, pOpeningTagEnd);
-
-		if (!good) break;
-
-		pClosingTagStart = pOpeningTagEnd;
-	
-		const char * endTag = this -> pEndTag;
-		ptemp = pClosingTagStart;
-		tagSize = 0;
-
-		while ( *(endTag) != '\0') {
-			if ( *(ptemp) == '\0') {
-				this -> lastError = END_OF_DATA;
-				return false;
-			}
-			ptemp++;
-			tagSize++; //might not need tagsize anymore
-			endTag++;
-		}
-
-		/*char * ptemp = pClosingTagStart;
-		int i = tagSize;
-		while (i != 1) {
-			if ( *(ptemp) == '\0') {
-				this -> lastError = END_OF_DATA;
-				break;
-			}
-			ptemp++;
-			i--;
-		}*/
-
-		//ptemp = closingTagStart + (tagSize - 1)
-		pClosingTagEnd = ptemp;
-		delete ptemp;
-
-		good = this -> findTag( this -> pEndTag, pClosingTagStart, pClosingTagEnd);
-	
-		if (!good) break;
-
-		
-
-		std::string data("");
-
-		char * pc;
-
-		for (pc = (++pOpeningTagEnd); pc != pClosingTagStart; pc++) { //just want the data between the end of the starting tag and the start of the closing tag
-			data += *(pc);
-		}
-		myvector.push_back(data);
-		
-		//maybe delete the defined pointers? No issue assigning pDataToSearchThru to a new pointer bc it was passed by value 
-
-		pDataToSearchThru = pClosingTagEnd; //we have already searched through all the other data before pClosingTagEnd might as well be the new starting point
-	}
-	//okay so this is a good start, but what if there are multiple opening and closing tag pairs, probably should 
-	// refactor lines 74 to 117 into a while loop that says something like
-			// while (findTags) since find tags will work until an invalid tag is found, but we can talk about that
-
-	return myvector.empty();
 }
 
 /*
 
 */
 void StringParserClass::cleanup() {
-	//not sure....
+	
 }
 
 /*
@@ -190,63 +112,5 @@ void StringParserClass::cleanup() {
 		End will be the last character of the tag
 */
 bool StringParserClass::findTag(char *tagToLookFor, char *&start, char *&end) {
-	//This method seems like a helper class to find tags in the data returning their starting and ending position
 	
-	bool tagFound = false;//a tag is found when start == '<' and end == '>' and the data between matches
-	char * temp = tagToLookFor;
-
-	while (*(temp) != '\0') temp++;
-	
-	char startChar = *(tagToLookFor);
-	char endChar = *(--temp);
-
-	if (this -> areTagsSet) //shouldn't happen anymore since there is a check before calling thsi function
-	{
-		temp = tagToLookFor;
-		start++;
-		while (	!tagFound ) {
-			
-			char startDref = *start;
-			
-			char endDref = *end;
-			if ( startDref == startChar && endDref == endChar) {
-				//don't need the < at the start of the tag
-				start++;
-				char  pStartOfTag = *(start);
-				temp++;
-				char  pEndOftag = *end;
-
-				while (pStartOfTag != pEndOftag) { // might need to deref these here, but i don't think so?
-					if ((pStartOfTag) != *(temp)) {
-						tagFound = false;
-						break; //time to look again
-					}
-					pStartOfTag++;
-					temp++;
-					tagFound = true;
-				}
-			} else {
-				temp = tagToLookFor;
-				start++;
-				end++;
-			}
-
-			if (*start == '\0' || *end == '\0') { // in this case start should never == '0', but it is a just in case8
-				this -> lastError = END_OF_DATA; //hit null term character no more data never found last tag
-				return false;
-			}
-		}
-	}
-	else
-	{
-		this -> lastError = ERROR_TAGS_NULL;
-		return false;
-	}
-
-	if(!tagFound) {
-		this -> lastError = NO_TAG_FOUND; //tags were not found
-		return false;
-	}
-
-	return true;
 }
