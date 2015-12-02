71389b8235716bb2ecc7e51bf4e61dfe10f0f7c0
juanvallejo <juan.vallejo.12@cnu.edu>
2015-12-01 21:40:06 -0500

    implement datastore.cpp

--- Project6_MED - Skeleton/DataStore/DataStore.cpp
+++ Project6_MED - Skeleton/DataStore/DataStore.cpp
@@ -3,34 +3,74 @@
 #include "DataStore.h"
 #include "../includes/Crypto_AES.h"
 
-//this is a comment
-DataStore::DataStore(Crypto* myCrypt):myCrypto(myCrypt){}
+/**
+ * Constructor takes a myCrypt object and assigns it as class field
+ * If no cyrpt object is passed, it is a assigned a null value
+ */
+DataStore::DataStore(Crypto* myCrypt = 0):myCrypto(myCrypt) {}
+
 DataStore::~DataStore(void){}
 
+/**
+ * @protected
+ * Called by save method to encript data being stored.
+ * Assumes myCrypto is not null, however will not throw error or return false otherwise.
+ *
+ * @return boolean true on completion of function scope.
+ */
 bool DataStore::encrypt(std::string &str) {
-	bool complete = false;
 
-	if (this->myCrypto != NULL) {
-		//do stuff
-		(*myCrypto).encrypt(str);// string now encrypted
-		complete = true;
-	} else {
-		// do nothing 
-		complete = true;
+	bool complete = true;
+
+	// encrypt string only if crypto object exists
+	if(this->myCrypto != NULL) {
+		(*myCrypto).encrypt(str);
+		return complete;
 	}
 
 	return complete;
 }
 
-bool DataStore::decrypt(std::string &str){
-	bool complete = false;
+/**
+* @protected
+* Called by load method to decrypt data being retrieved.
+* Assumes myCrypto is not null, however will not throw error or return false otherwise.
+*
+* @return boolean true on completion of function scope.
+*/
+bool DataStore::decrypt(std::string &str) {
 
+	bool complete = true;
+
+	// encrypt string only if crypto object exists
 	if (this->myCrypto != NULL) {
-		(*myCrypto).decrypt(str); //string decrypted
-		complete = true;
-	} else {
-		//do nothing
-		complete = true;
+		(*myCrypto).decrypt(str);
+		return complete;
 	}
+
 	return complete;
+}
+
+/**
+ * Iterates through vector of String_Data passed, decrypting each string
+ */
+bool DataStore::load(std::vector<String_Data>& myVector) {
+	
+	for(std::vector<String_Data>::iterator vectorData = myVector.begin(); vectorData != myVector.end(); vectorData++) {
+		decrypt(vectorData->serialize());
+	}
+
+	return true;
+
+}
+
+/**
+* Iterates through vector of String_Data passed, encrypting each string
+*/
+bool DataStore::save(std::vector<String_Data>& myVector) {
+
+	for(std::vector<String_Data>::iterator vectorData = myVector.begin(); vectorData != myVector.end(); vectorData++) {
+		encrypt(vectorData->serialize());
+	}
+
 }
